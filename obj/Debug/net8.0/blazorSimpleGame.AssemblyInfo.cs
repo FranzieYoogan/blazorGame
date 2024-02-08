f9beadb9edda0ff9ef4b2cdf711f6e020a0e3944
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-02-08 05:33:00 -0300

    designArray

--- obj/Debug/net8.0/blazorSimpleGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazorSimpleGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorSimpleGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf88f8865180fb3ff0f811c75492f711fb689f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46cf1818d6baaea2fbc4b98c1b12aba2455a0b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorSimpleGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorSimpleGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
